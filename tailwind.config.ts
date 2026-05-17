27d9ca9783a8916570485cba5034e6c09093f2b8
Cwhitey6 <colinwhite988@gmail.com>
2026-05-16 21:00:46 -0400

    Fix Tailwind v4 styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,9 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/styles/**/*.css',
   ],
   darkMode: 'class',
   theme: {
