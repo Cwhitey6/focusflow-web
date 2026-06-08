b4553eb897b06cab9cd0a82960997ba26e935f1b
Cwhitey6 <colinwhite988@gmail.com>
2026-06-08 02:06:43 -0400

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+
+        {/* PWA */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#7c6af7" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+
+        {/* iOS */}
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="FocusFlow" />
         <link rel="apple-touch-icon" href="/android-chrome-192x192.png" />
